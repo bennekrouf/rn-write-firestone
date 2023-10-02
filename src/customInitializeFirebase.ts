826de2b7230a907cc0e644245a8ee1775aa6e711
Mohamed <mohamed.bennekrouf@gmail.com>
2023-10-02 12:13:58 +0200

    Add log

--- src/customInitializeFirebase.ts
+++ src/customInitializeFirebase.ts
@@ -2,10 +2,10 @@ import firebase from '@react-native-firebase/app';
 
 export const customInitializeFirebase = (firebaseConfig:any) => {
   if (!firebase.apps.length) {
-    // console.log(`RN Init firebase app with : ${firebaseConfig}`);
+    console.log(`RN Init firebase app with : ${firebaseConfig}`);
     return firebase.initializeApp(firebaseConfig);
   } else {
-    // console.log(`RN Found firebase app : ${JSON.stringify(firebase.apps)}`);
+    console.log(`RN Found firebase app : ${JSON.stringify(firebase.apps)}`);
     return firebase.app();
   }
 };
