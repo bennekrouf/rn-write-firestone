b580261eb83b48a82ae79805e0f23a2faacb72b1
Mohamed <mohamed.bennekrouf@gmail.com>
2023-10-02 14:34:57 +0200

    Add logs

--- dist/customInitializeFirebase.js
+++ dist/customInitializeFirebase.js
@@ -6,13 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.customInitializeFirebase = void 0;
 const app_1 = __importDefault(require("@react-native-firebase/app"));
 const customInitializeFirebase = (firebaseConfig) => {
-    if (!app_1.default.apps.length) {
-        console.log(`RN Init firebase app with : ${firebaseConfig}`);
-        return app_1.default.initializeApp(firebaseConfig);
+    try {
+        if (!app_1.default.apps.length) {
+            console.log(`RN Init firebase app with : ${firebaseConfig}`);
+            return app_1.default.initializeApp(firebaseConfig);
+        }
+        else {
+            console.log(`RN Found firebase app : ${JSON.stringify(app_1.default.apps)}`);
+            return app_1.default.app();
+        }
     }
-    else {
-        console.log(`RN Found firebase app : ${JSON.stringify(app_1.default.apps)}`);
-        return app_1.default.app();
+    catch (error) {
+        console.log(`RN Init firebase app ERROR : ${error}`);
+        return error;
     }
 };
 exports.customInitializeFirebase = customInitializeFirebase;
