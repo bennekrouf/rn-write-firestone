b6b734884e9292b806aaa6384278cf6baa9e9fd2
Mohamed Bennekrouf <mohamed.bennekrouf@gmail.com>
2023-10-22 16:04:10 +0100

    Add logs

--- dist/customInitializeFirebase.js
+++ dist/customInitializeFirebase.js
@@ -7,19 +7,24 @@ exports.customInitializeFirebase = void 0;
 const app_1 = __importDefault(require("@react-native-firebase/app"));
 const rn_logging_1 = require("rn-logging");
 const customInitializeFirebase = (firebaseConfig) => {
+    if (!firebaseConfig || typeof firebaseConfig !== 'object' || Object.keys(firebaseConfig).length === 0) {
+        const errorMsg = 'Invalid or missing firebaseConfig provided.';
+        rn_logging_1.Logger.error(errorMsg, null, { tag: 'rn-write-firebase' });
+        throw new Error(errorMsg);
+    }
     try {
         if (!app_1.default.apps.length) {
-            rn_logging_1.Logger.info('Initializing firebase app', firebaseConfig, { tag: 'RN' });
+            rn_logging_1.Logger.info('Initializing firebase app', null, { tag: 'rn-write-firebase' });
             return app_1.default.initializeApp(firebaseConfig);
         }
         else {
-            rn_logging_1.Logger.info('Firebase app already initialized', { existingApps: app_1.default.apps }, { tag: 'RN' });
+            rn_logging_1.Logger.info('Firebase app already initialized', null, { tag: 'rn-write-firebase' });
             return app_1.default.app();
         }
     }
     catch (error) {
-        rn_logging_1.Logger.error('Error initializing firebase app', error, { tag: 'RN' });
-        return error;
+        rn_logging_1.Logger.error('Error initializing firebase app', error, { tag: 'rn-write-firebase' });
+        throw error;
     }
 };
 exports.customInitializeFirebase = customInitializeFirebase;
--- src/customInitializeFirebase.ts
+++ src/customInitializeFirebase.ts
@@ -1,17 +1,24 @@
 import firebase from '@react-native-firebase/app';
+
 import { Logger } from 'rn-logging';
 
-export const customInitializeFirebase = (firebaseConfig:any) => {
+export const customInitializeFirebase = (firebaseConfig: any): any => {
+  if (!firebaseConfig || typeof firebaseConfig !== 'object' || Object.keys(firebaseConfig).length === 0) {
+    const errorMsg = 'Invalid or missing firebaseConfig provided.';
+    Logger.error(errorMsg, null, { tag: 'rn-write-firebase' });
+    throw new Error(errorMsg);
+  }
+
   try {
     if (!firebase.apps.length) {
-      Logger.info('Initializing firebase app', firebaseConfig, { tag: 'RN' });
+      Logger.info('Initializing firebase app', null, { tag: 'rn-write-firebase' });
       return firebase.initializeApp(firebaseConfig);
     } else {
-      Logger.info('Firebase app already initialized', { existingApps: firebase.apps }, { tag: 'RN' });
+      Logger.info('Firebase app already initialized', null, { tag: 'rn-write-firebase' });
       return firebase.app();
     }
   } catch (error) {
-    Logger.error('Error initializing firebase app', error, { tag: 'RN' });
-    return error; 
+    Logger.error('Error initializing firebase app', error, { tag: 'rn-write-firebase' });
+    throw error; 
   }
 };
